4ea4fecac437e1c6f3edddda28dde54414b47492
Ushkov Fedor <fedor.ushkob22813@gmail.com>
2021-12-22 16:10:11 +0300

    first commit

---
+++ ex1.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ex1
+//
+//  Created by Федор on 22.12.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+    cout << a + b * 2 << endl;
+    return 0;
+}
---
+++ ex5/ex5/notimportant.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  ex5
+//
+//  Created by Федор on 22.12.2021.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[])
+{
+    int a = 0;
+    int b = 0;
+    cin >> a >> b;
+    cout << a - b << endl;
+    return 0;
+}
